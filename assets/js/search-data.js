607e282886b22ce4c312b3226119873b62e30c2f
dariant <darian.tomasevic@fri.uni-lj.si>
2025-02-18 14:55:28 +0000

    Deploying to gh-pages from @ dariant/dariant.github.io@544a7921730e049a99402575f548e727de714744 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,12 +4,19 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-about",
-    title: "About",
+    title: "about",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "post-a-post-with-image-galleries",
+  },{id: "nav-cv",
+          title: "cv",
+          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
+        },{id: "post-a-post-with-image-galleries",
       
         title: "a post with image galleries",
       
